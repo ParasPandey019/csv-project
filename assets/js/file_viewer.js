cf79d66a805f0d859a2cfa257c24718c8e05854f
ParasPandey019 <paraspandey019@gmail.com>
2023-10-11 14:32:53 +0530

    added pagination

--- assets/js/file_viewer.js
+++ assets/js/file_viewer.js
@@ -1,4 +1,4 @@
-document.getElementById("search").addEventListener("click", searchTable);
+document.getElementById("go").addEventListener("click", searchTable);
 
 function searchTable() {
   const input = document.getElementById("search-input").value.toLowerCase();
@@ -16,3 +16,78 @@ function searchTable() {
     }
   }
 }
+
+
+const itemsPerPage = 100;
+let currentPage = 1;
+
+let jsonData = [];
+function fetchData(){
+  const fileName = document.getElementById("file-name").innerText;
+  fetch(`/data/${fileName}`)
+  .then(response => response.json())
+  .then(data => {
+    jsonData = data;
+    console.log(jsonData);
+    renderTable(currentPage);
+    generatePaginationButtons();
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+}
+
+
+function renderTable(page) {
+  const tableBody = document.getElementById('table-body');
+  tableBody.innerHTML = ''; // Clear existing data
+
+  const start = (page - 1) * itemsPerPage;
+  const end = page * itemsPerPage;
+  const dataChunk = jsonData.slice(start, end);
+
+  for(let index in dataChunk){
+    const row = document.createElement('tr');
+    for(let key in dataChunk[index]) { 
+      const rowData = document.createElement('td');
+      rowData.innerText = dataChunk[index][key];
+      row.appendChild(rowData);
+     }
+    tableBody.appendChild(row);
+  };
+}
+
+function generatePaginationButtons() {
+  const pagination = document.getElementById('pagination');
+  pagination.innerHTML = '';
+
+  const totalPages = Math.ceil(jsonData.length / itemsPerPage);
+  const maxVisibleButtons = 5;
+  const halfVisibleButtons = Math.floor(maxVisibleButtons / 2);
+
+  let startPage = currentPage - halfVisibleButtons;
+  let endPage = currentPage + halfVisibleButtons;
+
+  if (startPage < 1) {
+    startPage = 1;
+    endPage = Math.min(totalPages, maxVisibleButtons);
+  }
+
+  if (endPage > totalPages) {
+    endPage = totalPages;
+    startPage = Math.max(1, totalPages - maxVisibleButtons + 1);
+  }
+
+  for (let i = startPage; i <= endPage; i++) {
+    const button = document.createElement('button');
+    button.textContent = i;
+    button.addEventListener('click', () => {
+      currentPage = i;
+      renderTable(currentPage);
+      generatePaginationButtons();
+    });
+    pagination.appendChild(button);
+  }
+}
+
+fetchData();
